5a30f06e5563f76fe706d736e88d77f3ac72a5a2
Koutaro Mukai <mukai.k1011k@outlook.jp>
2022-11-08 22:17:41 +0900

    Use classes

--- SerialController/LineNotify.py
+++ SerialController/LineNotify.py
@@ -1,26 +1,115 @@
 import configparser
+from datetime import datetime, timezone
+from typing import Dict, Optional
 import cv2
 import io
-import os
+from os import path
 
 import requests
 from PIL import Image
-from logging import Logger, getLogger, DEBUG, NullHandler
+from logging import getLogger, DEBUG, NullHandler
 
-def _get_token_list(filename: str):
-    proxy = _open_file_with_utf8(filename)['LINE']
-    return {key: proxy[key] for key in proxy}
+endpoints = {
+    "status": "https://notify-api.line.me/api/status",
+    "notify": "https://notify-api.line.me/api/notify"
+}
 
-def _open_file_with_utf8(filename: str):
+class _Token():
     """
-    utf-8 のファイルを BOM ありかどうかを自動判定して読み込む
+    LINE Notifyのトークンを表す
+    """
+    def __init__(self, token: str) -> None:
+        self.__token = token
+
+    def get_header(self) -> Dict[str, str]:
+        return {"Authorization": f"Bearer {self.__token}"}
+
+    def get_status(self, tz=datetime.utcnow().astimezone().tzinfo):
+        class _Status():
+            """
+            トークンの利用状況を表す
+            """
+            def __init__(self, token: _Token, res: requests.Response, tz: timezone) -> None:
+                self.__token = token
+
+                res.raise_for_status()
+                self.__status_code = res.status_code
+                self.__limit = res.headers["X-RateLimit-Limit"]
+                self.__image_limit = res.headers["X-RateLimit-ImageLimit"]
+                self.__remaining = res.headers["X-RateLimit-Remaining"]
+                self.__image_remaining = res.headers["X-RateLimit-ImageRemaining"]
+                self.__reset = datetime.fromtimestamp(int(res.headers["X-RateLimit-Reset"]), tz)
+
+            @property
+            def token(self) -> _Token:
+                return self.__token
+            @property
+            def status_code(self) -> int:
+                return self.__status_code
+            @property
+            def limit(self) -> str:
+                return self.__limit
+            @property
+            def image_limit(self) -> str:
+                return self.__image_limit
+            @property
+            def remaining(self) -> str:
+                return self.__remaining
+            @property
+            def image_remaining(self) -> str:
+                return self.__image_remaining
+            @property
+            def reset(self) -> datetime:
+                return self.__reset
+
+        res = requests.get(endpoints["status"], headers=self.get_header())
+        
+        # デフォルトのタイムゾーンをtzinfoから取得できない場合は、utcを用いる
+        if tz is None:
+            tz = timezone.utc
+
+        return _Status(self, res, tz)
+    
+class _Message():
+    """
+    LINE Notifyで送信するメッセージを表す
+    """
+    def __init__(self, message: str) -> None:
+        self.__message = message
+
+    def get_header(self) -> Dict[str, str]:
+        return {'Message': f'{self.__message}'}
+
+def _convert_image(image_bgr: cv2.Mat):
+    """
+    cv2.Matをbytesに変換する
+    """
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+    image = Image.fromarray(image_rgb)
+    png = io.BytesIO()  # 空のio.BytesIOオブジェクトを用意
+    image.save(png, format='png')  # 空のio.BytesIOオブジェクトにpngファイルとして書き込み
+    return png.getvalue()  # io.BytesIOオブジェクトをbytes形式で読みとり
+
+class _Image():
+    """
+    LINE Notifyで送信する画像を表す
+    """
+    def __init__(self, mat: cv2.Mat) -> None:
+        self.__bytes = _convert_image(mat)
+
+    def get_header(self) -> Dict[str, bytes]:
+        return {'imageFile': self.__bytes}
+
+def _get_tokens_from(filename: str) -> Dict[str, _Token]:
+    """
+    指定されたファイルからトークンの辞書を取得する
     """
     parser = configparser.ConfigParser(comment_prefixes='#', allow_no_value=True)
     parser.read(filename, 'utf-8-sig' if _is_utf8_file_with_bom(filename) else 'utf-8')
-    
-    return parser
+    proxy = parser['LINE']
+    return {key: _Token(proxy[key]) for key in proxy}
 
-def _is_utf8_file_with_bom(filename):
+def _is_utf8_file_with_bom(filename) -> bool:
     """
     utf-8 ファイルが BOM ありかどうかを判定する
     """
@@ -35,108 +124,90 @@ class Line_Notify:
         self._logger.addHandler(NullHandler())
         self._logger.setLevel(DEBUG)
         self._logger.propagate = True
-
-        self.__res = None
         
-        self._logger.debug("Load token file")
-        
-        TOKEN_FILE = os.path.join(os.path.dirname(__file__), 'line_token.ini')
-
         self.__camera = camera
-        self.__token_list = _get_token_list(TOKEN_FILE)
-        self.__token_num = len(self.__token_list)
-        # self.line_notify_token = self.token_file['LINE'][token_name]
-        self.__headers = [{'Authorization': f'Bearer {token}'} for key, token in self.__token_list.items()]
-        self.__res = [requests.get('https://notify-api.line.me/api/status', headers=head) for head in self.__headers]
-        self.__status = [responses.status_code for responses in self.__res]
-        self.__chk_token_json = [responses.json() for responses in self.__res]
+
+        self._logger.debug("Load token file")
+        TOKEN_FILE = path.join(path.dirname(__file__), 'line_token.ini')
+        self.__tokens = _get_tokens_from(TOKEN_FILE)
 
     def __str__(self):
-        for stat in self.__status:
-            if stat == 401:
-                self._logger.error("Invalid token")
-                return "LINE Token Check FAILED."
-            elif stat == 200:
-                self._logger.info("Valid token")
-                return "LINE-Token Check OK!"
+        try:
+            _ = [token.get_status() for token in self.__tokens.values()]
+        except:
+            self._logger.error("Invalid token")
+            return "LINE Token Check FAILED."
+        
+        self._logger.info("Valid token")
+        return "LINE-Token Check OK!"
 
-    def send_text(self, notification_message, token='token'):
+    def __send(self, token_key: str, notification_message: str, image: Optional[_Image]=None):
+        target = {
+            "jpn": "テキスト" if image is None else "テキストと画像",
+            "eng": "text" if image is None else "image with text"
+        }
+        try:
+            token = self.__tokens[token_key]
+        except KeyError:
+            print(f"指定されたトークンは登録されていません: {token_key}")
+            self._logger.error(f"Using the wrong token_key: {token_key}")
+            return
+        message = _Message(notification_message)
+
+        try:
+            if image is None:
+                res = requests.post(endpoints["notify"], headers=token.get_header(), data=message.get_header())
+            else:
+                res = requests.post(endpoints["notify"], headers=token.get_header(), params=message.get_header(), files=image.get_header())
+            res.raise_for_status()
+
+        except:
+            print(f"[LINE]{target['jpn']}の送信に失敗しました。")
+            self._logger.error(f"Failed to send {target['eng']}")
+            return
+        
+        print(f"[LINE]{target['jpn']}を送信しました。")
+        self._logger.info(f"Send {target['eng']}")
+
+    def send_text(self, notification_message, token_key='token'):
         """
         LINEにテキストを通知する
         """
-        line_notify_api = 'https://notify-api.line.me/api/notify'
-        try:
-            headers = {'Authorization': f'Bearer {self.__token_list[token]}'}
-            data = {'Message': f'{notification_message}'}
-            self.__res = requests.post(line_notify_api, headers=headers, data=data)
-            if self.__res.status_code == 200:
-                print("[LINE]テキストを送信しました。")
-                self._logger.info("Send text")
-            else:
-                print("[LINE]テキストの送信に失敗しました。")
-                self._logger.error("Failed to send text")
-        except KeyError:
-            print('token名が間違っています')
-            self._logger.error('Using the wrong token')
-
-    def send_text_n_image(self, notification_message, token='token'):
+        self.__send(token_key, notification_message)
+        
+    def send_text_n_image(self, notification_message, token_key='token'):
         """
         カメラが開いていないときはテキストのみを通知し、
         開いているときはテキストと画像を通知する
         """
-        try:
-            if self.__camera is None:
-                print("Camera is not Opened. Send text only.")
-                self.send_text(notification_message)
-                return
-
-            image_bgr = self.__camera.readFrame()
-            image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
-            image = Image.fromarray(image_rgb)
-            png = io.BytesIO()  # 空のio.BytesIOオブジェクトを用意
-            image.save(png, format='png')  # 空のio.BytesIOオブジェクトにpngファイルとして書き込み
-            b_frame = png.getvalue()  # io.BytesIOオブジェクトをbytes形式で読みとり
-
-            line_notify_api = 'https://notify-api.line.me/api/notify'
-            headers = {'Authorization': f'Bearer {self.__token_list[token]}'}
-            data = {'Message': f'{notification_message}'}
-            files = {'imageFile': b_frame}
-            self.__res = requests.post(line_notify_api, headers=headers, params=data, files=files)
-            if self.__res.status_code == 200:
-                print("[LINE]テキストと画像を送信しました。")
-                self._logger.info("Send image with text")
-            else:
-                print("[LINE]テキストと画像の送信に失敗しました。")
-                self._logger.error("Failed to send image with text")
-        except KeyError:
-            print('token名が間違っています')
-            self._logger.error('Using the wrong token')
+        if self.__camera is None:
+            print("Camera is not Opened. Send text only.")
+            self.send_text(notification_message, token_key)
+            return
+        
+        self.__send(token_key, notification_message, _Image(self.__camera.readFrame()))
 
     def getRateLimit(self):
-        try:
-            for i in range(self.__token_num):
-                print(f'For: {list(self.__token_list.keys())[i]}')
-                print('X-RateLimit-Limit: ' + self.__res[i].headers['X-RateLimit-Limit'])
-                print('X-RateLimit-ImageLimit: ' + self.__res[i].headers['X-RateLimit-ImageLimit'])
-                print('X-RateLimit-Remaining: ' + self.__res[i].headers['X-RateLimit-Remaining'])
-                print('X-RateLimit-ImageRemaining: ' + self.__res[i].headers['X-RateLimit-ImageRemaining'])
-                import datetime
-                dt = datetime.datetime.fromtimestamp(int(self.__res[i].headers['X-RateLimit-Reset']),
-                                                     datetime.timezone(datetime.timedelta(hours=9)))
-                print('Reset time:', dt, '\n')
+        for key, token in self.__tokens.items():
+            try:
+                status = token.get_status()
+            except:
+                # self._logger.error(f"Invalid token: {key}")
+                continue
 
-                self._logger.info(f"LINE API - Limit: {self.__res[i].headers['X-RateLimit-Limit']}")
-                self._logger.info(f"LINE API - Remaining: {self.__res[i].headers['X-RateLimit-Remaining']}")
-                self._logger.info(f"LINE API - ImageLimit: {self.__res[i].headers['X-RateLimit-Limit']}")
-                self._logger.info(f"LINE API - ImageRemaining: {self.__res[i].headers['X-RateLimit-ImageRemaining']}")
-                self._logger.info(f"Reset time: {dt}")
-        except AttributeError as e:
-            self._logger.error(e)
-            pass
-        except KeyError as e:
-            self._logger.error(e)
-            pass
+            print(f"For: {key}")
+            print(f"X-RateLimit-Limit: {status.limit}")
+            print(f"X-RateLimit-ImageLimit: {status.image_limit}")
+            print(f"X-RateLimit-Remaining: {status.remaining}")
+            print(f"X-RateLimit-ImageRemaining: {status.image_remaining}")
+            print(f"X-RateLimit-Reset: {status.reset}")
+            print()
 
+            self._logger.info(f"LINE API - Limit: {status.limit}")
+            self._logger.info(f"LINE API - ImageLimit: {status.image_limit}")
+            self._logger.info(f"LINE API - Remaining: {status.remaining}")
+            self._logger.info(f"LINE API - ImageRemaining: {status.image_remaining}")
+            self._logger.info(f"LINE API - Reset: {status.reset}")
 
 if __name__ == "__main__":
     '''
