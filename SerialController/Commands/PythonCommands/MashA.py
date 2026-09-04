2b7c7105cff543de60facb45dd81a6d55e3b9964
moi_poke <moi.lovegood+poke@gmail.com>
2026-09-04 11:49:02 +0900

    fix(sample): stick timing tweaks, mash button behavior

--- SerialController/Commands/PythonCommands/MashA.py
+++ SerialController/Commands/PythonCommands/MashA.py
@@ -8,12 +8,15 @@ from Commands.PythonCommandBase import PythonCommand
 # Mash a button A
 # A連打
 class Mash_A(PythonCommand):
-    NAME = 'A連打'
+    NAME = "A連打"
 
     def __init__(self):
         super().__init__()
 
     def do(self):
+        self.hold(Button.ZR)
         while True:
-            self.wait(0.5)
-            self.press(Button.A)
+            self.press(Button.A, wait=0, duration=0.005)
+            self.press(Button.B, wait=0, duration=0.005)
+            self.press(Button.X, wait=0, duration=0.005)
+            self.press(Button.Y, wait=0, duration=0.005)
